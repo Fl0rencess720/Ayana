2799b2e7a779c62973623681f6c9515546781b1f
Fl0rencess720 <2391793239@qq.com>
2025-04-17 17:51:58 +0800

    fix: 解决User自动迁移报错

--- app/service/user/internal/data/model.go
+++ app/service/user/internal/data/model.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Phone    string `gorm:"type:varchar(20);uniqueIndex"`
+	Phone    string `gorm:"type:varchar(20);index"`
 	Password string
 	Name     string
 	Avatar   string
